dd709cdbe90576100e754911625493717c5b5eb0
Mohammed Raseeth J <mohammedraseeth47@gmail.com>
2026-08-21 07:04:40 +0530

    Delete trackgo_passenger directory

--- trackgo_passenger/android/app/src/main/kotlin/com/example/trackgo_passenger/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.trackgo_passenger
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- trackgo_passenger/lib/core/providers/storage_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../services/local_storage_service.dart';
-
-/// Provider for LocalStorageService instance.
-/// Overridden in main.dart once SharedPreferences initializes.
-final localStorageServiceProvider = Provider<LocalStorageService>((ref) {
-  throw UnimplementedError('localStorageServiceProvider has not been initialized');
-});
